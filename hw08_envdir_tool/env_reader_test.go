444220bf3ed17594d8ce939508848e28626bed65
MSan <mikhail.saratov@vk.com>
2022-07-16 22:08:23 +0400

    HW08 before linters

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,68 @@
 package main
 
 import "testing"
+import "github.com/stretchr/testify/require"
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+
+	t.Run("no directory", func(t *testing.T) {
+		env, err := ReadDir("dir_does_not_exist")
+
+		require.Error(t, err)
+		require.Nil(t, env)
+	})
+
+	t.Run("empty directory", func(t *testing.T) {
+		env, err := ReadDir("testdata/emptydir")
+
+		require.Nil(t, err)
+		require.Len(t, env, 0)
+	})
+
+	t.Run("dir inside", func(t *testing.T) {
+		env, err := ReadDir("testdata/dirinside")
+
+		require.Nil(t, err)
+		require.Len(t, env, 1, "nested dir not skipped")
+	})
+
+	t.Run("read simple files", func(t *testing.T) {
+		env, err := ReadDir("testdata/simple")
+
+		require.Nil(t, err)
+		require.Len(t, env, 3)
+		require.True(t, env["todelete"].NeedRemove, "delete flag reading fail")
+		require.Equal(t, "001", env["one"].Value, "one string reading fail")
+		require.Equal(t, "002", env["two"].Value, "multiple string reading fail")
+	})
+}
+
+func TestEvaluateEnvValue(t *testing.T) {
+	tests := []struct {
+		name, in, out string
+	}{
+		{
+			name: "regular string",
+			in:   "/usr/local/go",
+			out:  "/usr/local/go",
+		},
+		{
+			name: "0x00 replacement",
+			in:   "fullpath\x00notincluded",
+			out:  "fullpath\nnotincluded",
+		},
+		{
+			name: "tabs and spaces",
+			in:   "/usr/local/go\t\t \r\r  \t",
+			out:  "/usr/local/go",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			res := evaluateEnvValue([]byte(test.in))
+			require.Equal(t, test.out, res)
+		})
+	}
+
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,63 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+	"strings"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	// 1: prepare args and exec
+	var args []string
+	if len(cmd) > 1 {
+		args = cmd[1:]
+	}
+
+	e := exec.Command(cmd[0], args...)
+	e.Stdout = os.Stdout
+	e.Stdin = os.Stdin
+	e.Stderr = os.Stderr
+
+	// 2: edit envs
+	e.Env = prepareEnvs(os.Environ(), env)
+
+	// 3: run and exit code
+	err := e.Start()
+	if err != nil {
+		return e.ProcessState.ExitCode()
+	}
+
+	err = e.Wait()
+	if err != nil {
+		if exerr, ok := err.(*exec.ExitError); ok {
+			return exerr.ProcessState.ExitCode()
+		}
+	}
+
+	return 0
+}
+
+func prepareEnvs(globalEnvs []string, editions Environment) []string {
+	envs := make(map[string]string)
+
+	for i := range globalEnvs {
+		row := strings.Split(globalEnvs[i], "=")
+		envs[row[0]] = row[1]
+	}
+
+	for k, v := range editions {
+		if v.NeedRemove {
+			delete(envs, k)
+			continue
+		}
+		envs[k] = v.Value
+	}
+
+	res := make([]string, 0)
+	for k, v := range envs {
+		res = append(res, k+"="+v)
+	}
+
+	return res
 }
