e230a1480bca4a5973200a4a19a1ccf91977c237
MSan <mikhail.saratov@vk.com>
2022-07-23 20:02:39 +0400

    HW09 initial solution

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,14 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -8,10 +17,296 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+
+	b := strings.Builder{}
+	for _, e := range v {
+		b.WriteString(e.Error())
+		b.WriteRune('\n')
+	}
+	return b.String()
 }
 
+func (e ValidationError) Error() string {
+	return fmt.Sprintf("%v in field %v", e.Err, e.Field)
+}
+
+func (e ValidationError) Unwrap() error {
+	return e.Err
+}
+
+const validateTagKey = "validate"
+
+// processing errors
+var (
+	errNotAStruct            = errors.New("Not a struct")
+	errNotSupportedFieldType = errors.New("Not supported field type")
+	errWrongValidationType   = errors.New("Wrong validation type ")
+	errWrongValidationValue  = errors.New("Wrong validation value ")
+)
+
+// validation errors
+var (
+	errValidation = errors.New("Invalid")
+	errMin        = fmt.Errorf("%w min: less then", errValidation)
+	errMax        = fmt.Errorf("%w max: greater then", errValidation)
+	errIn         = fmt.Errorf("%w in: not in", errValidation)
+	errLen        = fmt.Errorf("%w len: not equal", errValidation)
+	errRegex      = fmt.Errorf("%w regexp: not match with", errValidation)
+)
+
 func Validate(v interface{}) error {
-	// Place your code here.
+	val := reflect.ValueOf(v)
+	typ := val.Type()
+
+	if typ.Kind() != reflect.Struct {
+		return errNotAStruct
+	}
+
+	valErrors := make(ValidationErrors, 0)
+	for fnum := 0; fnum < typ.NumField(); fnum++ {
+		tagstring, ok := typ.Field(fnum).Tag.Lookup(validateTagKey)
+		if !ok {
+			continue
+		}
+
+		tags := strings.Split(tagstring, "|")
+		for _, tag := range tags {
+			valtag := strings.SplitN(tag, ":", 2)
+
+			err := validateTag(val.Field(fnum), typ.Field(fnum).Name, valtag[0], valtag[1])
+
+			var valTagErrors ValidationErrors
+			var valTagError ValidationError
+
+			if errors.As(err, &valTagError) {
+				valErrors = append(valErrors, valTagError)
+				continue
+			}
+
+			if errors.As(err, &valTagErrors) {
+				valErrors = append(valErrors, valTagErrors...)
+				continue
+			}
+
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	if len(valErrors) != 0 {
+		return valErrors
+	}
 	return nil
+}
+
+func validateTag(v reflect.Value, name, valType, valValue string) error {
+	var err error
+
+	switch v.Kind() {
+	case reflect.Int:
+		f, makeErr := makeIntValidator(name, valType, valValue)
+		if makeErr != nil {
+			return makeErr
+		}
+		t := int(v.Int())
+		err = f(t)
+
+	case reflect.String:
+		f, makeErr := makeStringValidator(name, valType, valValue)
+		if makeErr != nil {
+			return makeErr
+		}
+		t := v.String()
+
+		err = f(t)
+
+		//TODO дополнить разбор слайса обёрток
+	case reflect.Slice:
+		if t, ok := v.Interface().([]int); ok {
+			f, makeErr := makeIntValidator(name, valType, valValue)
+			if makeErr != nil {
+				return makeErr
+			}
+			err = validateIntSlice(t, f)
+			break
+		}
+
+		if t, ok := v.Interface().([]string); ok {
+			f, makeErr := makeStringValidator(name, valType, valValue)
+			if makeErr != nil {
+				return makeErr
+			}
+			err = validateStringSlice(t, f)
+			break
+		}
+		err = errNotSupportedFieldType
+	default:
+		err = errNotSupportedFieldType
+	}
+
+	return err
+}
+
+func validateIntSlice(arr []int, f func(int) error) error {
+	var errs ValidationErrors
+	for _, x := range arr {
+		err := f(x)
+		if err == nil {
+			continue
+		}
+		e, ok := err.(ValidationError)
+		if !ok {
+			return err
+		}
+		errs = append(errs, e)
+	}
+	if len(errs) == 0 {
+		return nil
+	}
+	return errs
+}
+
+func validateStringSlice(arr []string, f func(string) error) error {
+	var errs ValidationErrors
+	for _, x := range arr {
+		err := f(x)
+		if err == nil {
+			continue
+		}
+		e, ok := err.(ValidationError)
+		if !ok {
+			return err
+		}
+		errs = append(errs, e)
+	}
+	if len(errs) == 0 {
+		return nil
+	}
+	return errs
+}
+
+func makeIntValidator(name, tag, value string) (func(int) error, error) {
+	var f func(int) error
+	switch tag {
+	case "min":
+		intvalue, err := strconv.Atoi(value)
+		if err != nil {
+			return nil, fmt.Errorf("%w tag %v value %v", errWrongValidationValue, tag, value)
+		}
+
+		f = func(i int) error {
+			if i < intvalue {
+				return ValidationError{
+					Field: name,
+					Err:   fmt.Errorf("%w %v", errMin, value),
+				}
+			}
+			return nil
+		}
+
+	case "max":
+		intvalue, err := strconv.Atoi(value)
+		if err != nil {
+			return nil, fmt.Errorf("%w tag %v value %v", errWrongValidationValue, tag, value)
+		}
+
+		f = func(i int) error {
+			if i > intvalue {
+				return ValidationError{
+					Field: name,
+					Err:   fmt.Errorf("%w %v", errMax, value),
+				}
+			}
+			return nil
+		}
+
+	case "in":
+		values := strings.SplitN(value, ",", 2)
+		min, err := strconv.Atoi(values[0])
+		if err != nil {
+			return nil, fmt.Errorf("%w tag %v value %v", errWrongValidationValue, tag, value)
+		}
+		max, err := strconv.Atoi(values[1])
+		if err != nil {
+			return nil, fmt.Errorf("%w tag %v value %v", errWrongValidationValue, tag, value)
+		}
+
+		f = func(i int) error {
+			if i < min || i > max {
+				return ValidationError{
+					Field: name,
+					Err:   fmt.Errorf("%w %v", errIn, value),
+				}
+			}
+			return nil
+		}
+
+	default:
+		return nil, fmt.Errorf("%w %v", errWrongValidationType, value)
+
+	}
+
+	return f, nil
+}
+
+func makeStringValidator(name, tag, value string) (func(string) error, error) {
+	var f func(string) error
+
+	switch tag {
+	case "len":
+		intvalue, err := strconv.Atoi(value)
+		if err != nil {
+			return nil, fmt.Errorf("%w tag %v value %v", errWrongValidationValue, tag, value)
+		}
+
+		f = func(s string) error {
+			if len(s) != intvalue {
+				return ValidationError{
+					Field: name,
+					Err:   fmt.Errorf("%w %v", errLen, value),
+				}
+			}
+			return nil
+		}
+
+	case "regexp":
+		re, err := regexp.Compile("^" + value + "$") // full string matching
+		if err != nil {
+			return nil, fmt.Errorf("%w, regexp not compiled: %v", errWrongValidationValue, value)
+		}
+
+		f = func(s string) error {
+			if re.MatchString(s) {
+				return nil
+			}
+
+			fmt.Println("")
+			return ValidationError{
+				Field: name,
+				Err:   fmt.Errorf("%w %v", errRegex, value),
+			}
+		}
+
+	case "in":
+		values := strings.Split(value, ",")
+
+		f = func(s string) error {
+			for i := range values {
+				if values[i] == s {
+					return nil
+				}
+			}
+
+			return ValidationError{
+				Field: name,
+				Err:   fmt.Errorf("%w %v", errIn, value),
+			}
+		}
+
+	default:
+		return nil, fmt.Errorf("%w %v", errWrongValidationType, value)
+	}
+
+	return f, nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -3,6 +3,8 @@ package hw09structvalidator
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/stretchr/testify/require"
+	_ "github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -11,7 +13,7 @@ type UserRole string
 // Test the function on different structures and other types.
 type (
 	User struct {
-		ID     string `json:"id" validate:"len:36"`
+		ID     string `json:"id" validate:"len:10"`
 		Name   string
 		Age    int      `validate:"min:18|max:50"`
 		Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
@@ -24,6 +26,18 @@ type (
 		Version string `validate:"len:5"`
 	}
 
+	AppR struct {
+		Version string `validate:"regexp:\\d+"`
+	}
+
+	AppI struct {
+		Version string `validate:"in:one,two,three"`
+	}
+
+	Num struct {
+		Number int `validate:"min:10|max:50|in:20,60"`
+	}
+
 	Token struct {
 		Header    []byte
 		Payload   []byte
@@ -34,18 +48,136 @@ type (
 		Code int    `validate:"in:200,404,500"`
 		Body string `json:"omitempty"`
 	}
+
+	Password struct {
+		FirstLetter  string `validate:"len:1"`
+		OtherLetters string `validate:"notin:werty,23456"`
+	}
 )
 
 func TestValidate(t *testing.T) {
 	tests := []struct {
-		in          interface{}
-		expectedErr error
+		in                     interface{}
+		expectedErr            error
+		expectedValidationErrs []error
+		fails                  []string
 	}{
+		// no checks
 		{
-			// Place your code here.
+			in: Token{
+				Header:    nil,
+				Payload:   []byte("123123123123123"),
+				Signature: nil,
+			},
+			expectedErr:            nil,
+			expectedValidationErrs: nil,
 		},
-		// ...
-		// Place your code here.
+
+		// simple
+		{
+			in:                     App{Version: "12345"},
+			expectedErr:            nil,
+			expectedValidationErrs: nil,
+			fails:                  nil,
+		},
+		{
+			in:                     App{Version: "1234"},
+			expectedValidationErrs: []error{errLen},
+			fails:                  []string{"Version", "5"},
+		},
+		{
+			in:                     AppR{Version: "1234567890"},
+			expectedErr:            nil,
+			expectedValidationErrs: nil,
+			fails:                  nil,
+		},
+		{
+			in:                     AppR{Version: "ASDF 2.5"},
+			expectedErr:            nil,
+			expectedValidationErrs: []error{errRegex},
+			fails:                  []string{"d+", "Version"},
+		},
+		{
+			in:                     AppI{Version: "two"},
+			expectedErr:            nil,
+			expectedValidationErrs: nil,
+			fails:                  nil,
+		},
+		{
+			in:                     AppI{Version: "four"},
+			expectedErr:            nil,
+			expectedValidationErrs: []error{errIn},
+			fails:                  []string{"one,two,three", "Version"},
+		},
+		{
+			in:                     Num{Number: 30},
+			expectedErr:            nil,
+			expectedValidationErrs: nil,
+			fails:                  nil,
+		},
+		{
+			in:                     Num{Number: 100},
+			expectedErr:            nil,
+			expectedValidationErrs: []error{errMax, errIn},
+			fails:                  []string{"20,60", "50", "Number"},
+		},
+		{
+			in:                     Num{Number: -1},
+			expectedErr:            nil,
+			expectedValidationErrs: []error{errMin, errIn},
+			fails:                  []string{"20,60", "10", "Number"},
+		},
+		// complex
+		{
+			in: User{
+				ID:     "0000000000",
+				Name:   "Means Noth",
+				Age:    24,
+				Email:  "means@noth.io",
+				Role:   "admin",
+				Phones: []string{"00000000000", "11111111111"},
+				meta:   nil,
+			},
+			expectedErr:            nil,
+			expectedValidationErrs: nil,
+			fails:                  nil,
+		},
+		{
+			in: User{
+				ID:     "000000000015",
+				Name:   "",
+				Age:    118,
+				Email:  "means.noth.io",
+				Role:   "nice_guy",
+				Phones: []string{"000000000001", "1111"},
+				meta:   nil,
+			},
+			expectedErr:            nil,
+			expectedValidationErrs: []error{errLen, errMax, errRegex, errIn, errLen, errLen},
+			fails:                  []string{"ID", "Email", "Role", "Phones"},
+		},
+
+		// test software errors
+		{
+			in: Response{
+				Code: 250,
+				Body: "<resp>",
+			},
+			expectedErr: errWrongValidationValue,
+			fails:       []string{"404,505"},
+		},
+		{
+			in:                     ValidationErrors{ValidationError{Err: nil, Field: "myField"}},
+			expectedErr:            errNotSupportedFieldType,
+			expectedValidationErrs: nil,
+			fails:                  nil,
+		},
+		{
+			in:                     Password{FirstLetter: "Q", OtherLetters: "werty"},
+			expectedErr:            errWrongValidationType,
+			expectedValidationErrs: nil,
+			fails:                  []string{"nolen", "OtherLetters"},
+		},
 	}
 
 	for i, tt := range tests {
@@ -53,8 +185,33 @@ func TestValidate(t *testing.T) {
 			tt := tt
 			t.Parallel()
 
-			// Place your code here.
-			_ = tt
+			err := Validate(tt.in)
+			if tt.expectedErr == nil && len(tt.expectedValidationErrs) == 0 {
+				require.NoError(t, err)
+				return
+			}
+
+			// check if software error
+			if len(tt.expectedValidationErrs) == 0 {
+				e := tt.expectedErr
+				require.ErrorAs(t, err, &e)
+				return
+			}
+
+			// check validation errors
+			var l ValidationErrors
+			require.ErrorAs(t, err, &l)
+			require.Equal(t, len(tt.expectedValidationErrs), len(l), "Unexpected number of validation errors")
+
+			for _, e := range tt.expectedValidationErrs {
+				require.ErrorAs(t, err, &e)
+			}
+
+			// check expected words present
+			for _, f := range tt.fails {
+				require.ErrorContains(t, err, f)
+			}
+
 		})
 	}
 }
