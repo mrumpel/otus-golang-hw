7a1a44d65dea73507f619b191e52d1eb9b0cb018
MSan <mikhail.saratov@vk.com>
2022-08-09 08:09:16 +0400

    initial soluton

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,64 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	// 1: parse flags and parameters
+	var t time.Duration
+	flag.DurationVar(&t, "timeout", 0, "setup tcp connection timeout")
+	flag.Parse()
+
+	if flag.NArg() != 2 {
+		fmt.Println("error in set of parameters, need 2: host port")
+		os.Exit(1)
+	}
+
+	addr := net.JoinHostPort(flag.Arg(0), flag.Arg(1))
+
+	// 2: subscribe signal
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT)
+
+	// 3: create client + establish connection
+	client := NewTelnetClient(addr, t, os.Stdin, os.Stdout)
+
+	err := client.Connect()
+	defer client.Close()
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, "error in connection: ", err)
+		os.Exit(1)
+	}
+	_, _ = fmt.Fprintln(os.Stderr, "...Connected to", addr)
+
+	// 4: run writer and reader
+	go func() {
+		defer cancel()
+		err := client.Receive()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		_, _ = fmt.Fprintln(os.Stderr, "...Connection was closed by peer")
+	}()
+
+	go func() {
+		defer cancel()
+		err := client.Send()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		_, _ = fmt.Fprintln(os.Stderr, "...EOF")
+	}()
+
+	// 5: wait till the end
+	<-ctx.Done()
 }
